48c40e6861eb741ecc5b8e0a2b67c417a3915595
Gumindoga <bmuvindiguvukuvu@gmail.com>
2023-03-21 04:14:39 -0700

    Signed Int

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,22 @@
 #include <time.h>
 
 /**
- * main - assigns a random number to n and prints whether it's positive/negative
+ * main - assigns a random number to n and prints whether it's +/-ve
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+if (n > 0)
+  printf("%d is positive\n", n);
+else if (n == 0)
+  printf("%d is zero\n", n);
+else
+  printf("%d is negative\n", n);
 
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n == 0)
-        printf("%d is zero\n", n);
-    else
-        printf("%d is negative\n", n);
-
-    return (0);
+return (0);
 }
