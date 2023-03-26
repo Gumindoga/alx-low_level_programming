78872ae3d249d5e721d0bf97e482c8def2764d07
Gumindoga <bmuvindiguvukuvu@gmail.com>
2023-03-26 13:39:51 -0700

    Add task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+
+    i = 0;
+    while (i <= 7)
+    {
+        j = i + 1;
+        while (j <= 8)
+        {
+            k = j + 1;
+            while (k <= 9)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
