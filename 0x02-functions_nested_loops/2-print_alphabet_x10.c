c6fc85f8ff9849c2bfce247349eb4859fdf358de
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-28 23:48:07 +0200

    Fixing indent

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
 void print_alphabet_x10(void)
 {
   int count;
-  char c = 'a';
   
   for (count = 1; count <= 10; count++)
   {
+    char c = 'a';
     while (c <= 'z')
     {
       _putchar(c);
