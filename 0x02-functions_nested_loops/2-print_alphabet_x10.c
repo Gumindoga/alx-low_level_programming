8fcdfc2ccc329b56444beb1254b9e6358dc2282b
Gumindoga <bmuvindiguvukuvu@gmail.com>
2023-03-28 08:42:57 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet10x - write alpha 10x in lower
+ * print_alphabet_x10 - write alpha 10x in lower
  *
  * Return: 0;
  */
 
-void print_alphabet10x(void)
+void print_alphabet_x10(void)
 {
   int count = 1;
   char c = 'a';
 
-  while (count <= 10)
+  while (count <= 10) {
     while (c <= 'z') {
       _putchar(c);
       c++;
     }
+    
     count++;
+  }
+  
   _putchar('\n');
 }
