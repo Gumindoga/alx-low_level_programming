002a98ba8d3f2880c2828de95d917280ce5a387d
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 01:00:00 +0200

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ * @n: number to be checked
+ * 
+ * Return: 1 if +, 0 if 0 & -1 if -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
