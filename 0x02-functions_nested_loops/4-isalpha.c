a792f07156a215d3d5c6960b60c720d19b205c6b
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 00:33:48 +0200

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to check
- * Return: 1 if alphabet, 0 otherwise  
+ * Return: 1 if alphabet, 0 otherwise
  */
 
 int _isalpha(int c)
