540dccddcfdc9468a7006f4d853191ab6fc19c1d
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 16:30:55 +0200

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
 void print_times_table(int n)
 {
 	int i, j, product;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -26,7 +27,7 @@ void print_times_table(int n)
 					_putchar(product / 100 + '0');
 				if (product >= 10)
 					_putchar((product / 10) % 10 + '0');
-   			_putchar(product % 10 + '0');
+				_putchar(product % 10 + '0');
 			}
 			_putchar('\n');
 		}
