964f0d1352d20e68f658abd6bd59db5b81fd7a56
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 14:57:45 +0200

    task 11

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the n times table
+ * @n: integer limit of times table
+ */
+
+void times_table(int n)
+{
+	int i, j, p;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				p = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (p >= 10)
+				{
+					_putchar(p / 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+				else
+				{
+					if (j != 0)
+						_putchar(' ');
+					_putchar(p + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
