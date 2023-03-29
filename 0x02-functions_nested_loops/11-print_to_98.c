291b170e7223941b1ca9cbf6d3009879867095be
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 14:32:46 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 				if (n <= -100)
 					_putchar(-(n / 100) + '0');
 				if (n <= -10)
-					_putchar(-(n / 10) + '0');
+					_putchar(-(n / 10) % 10 + '0');
 				_putchar(-(n % 10) + '0');
 			}
 			else
