0feaee43e63cd482157ab7d9cdcefc715f419987
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 14:22:18 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,57 +1,45 @@
 #include "main.h"
-
 /**
  * print_to_98 - prints natural numbers to 98
  * @n: starting point integer
  */
-
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	if (n <= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (; n <= 98; n++)
 		{
-			if (i != n)
+			if (n < 0)
 			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i >= 10)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
+				_putchar('-');
+				if (n <= -10)
+					_putchar(-(n / 10) + '0');
+				_putchar(-(n % 10) + '0');
 			}
 			else
-				_putchar(i + '0');
+			{
+				if (n >= 10)
+					_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			if (n != 98)
+				_putchar(', ');
 		}
-		_putchar('\n');
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (; n >= 98; n--)
 		{
-			if (i != n)
+			if (n >= 100)
+				_putchar(n / 100 + '0');
+			if (n >= 10)
+				_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n != 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(', ');
 			}
-			if (i < 0)
-			{
-				_putchar('-');
-				if (i <= -10)
-					_putchar(-(i / 10) + '0');
-				_putchar(-(i % 10) + '0');
-			}
-			if (i >= 10)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			else
-				_putchar(i + '0');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
