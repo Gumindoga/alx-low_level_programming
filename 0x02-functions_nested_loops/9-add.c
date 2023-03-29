4a6b4cc77143df6e4d7705cad6d1ea133c7cf11c
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-29 12:52:23 +0200

    task 10

---
+++ 0x02-functions_nested_loops/9-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * Return: sum of two integers
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+	int sum;
+
+	sum = a + b;
+
+	_putchar(sum + '0');
+
+	return (sum);
+}
