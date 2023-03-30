0de39efeff2ecc2ae5a42e9d8ac184d40c27eead
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-30 12:46:00 +0200

    task 14

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - calculates sum of even fibonacci elements
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a;
+	long int b;
+	long int sum;
+	long int temp;
+
+	a = 1;
+	b = 2;
+
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+		{
+			sum = sum + b;
+		}
+		temp = a;
+		a = b;
+		b = temp + b;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
