1e1684ae668081ae966de35c5bb20c5ff3db0969
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-03-30 10:09:16 +0200

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,7 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0)
-			sum = sum + i;
-		if (i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
