bb0085946a158858b28678bb4894ab3dacf3d42b
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-01 13:23:51 +0200

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,26 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
-	}
+
+		if (i != 100)
+		{
+			printf(" ")
+		}
+		}
 	printf("\n");
 	return (0);
 }
