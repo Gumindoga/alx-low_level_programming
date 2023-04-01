e3b825675f982a5f0d1e44174a10cc01fa4688a1
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-01 17:59:42 +0200

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,18 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	else
+		num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
 }
