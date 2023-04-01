99452eb8734ceb1a0f6d4f0e47135709d64d5e48
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-01 10:58:33 +0200

    Edit Task 3 + Add Task 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		int c;
+
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + 'c');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
