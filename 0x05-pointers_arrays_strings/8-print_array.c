0e3dea100cf052be030fbeecd1b30475023d7c68
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-07 14:06:09 +0200

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i])
+		}
 	}
 
 	_putchar('\n');
