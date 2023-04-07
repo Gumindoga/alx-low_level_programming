891d0202b54dc17aae3722ba06daca8590e5ac0a
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-07 11:03:53 +0200

    Task 4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: char element in string
+ * Return: void
+ */
+
+void prev_string(char *s)
+{
+	int l;
+	int i;
+	char *t = "";
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+
+	i = l - 1;
+	while (i >= 0)
+	{
+		*t = *t + s[i];
+		i--;
+	}
+	_putchar('\n');
+}
