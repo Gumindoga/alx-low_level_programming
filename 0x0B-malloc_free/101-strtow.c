8122405d530e2e59f9f687760abc903a9a6bfb5d
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-13 02:52:09 +0200

    Final Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -49,16 +49,13 @@ char **strtow(char *str)
 
 	if (array == NULL)
 		return (NULL);
-
 	for (i = 0, j = 0; i < words; i++)
 	{
 		while (str[j] == ' ')
 			j++;
-
 		len = 0;
 		while (str[j + len] != ' ' && str[j + len] != '\0')
 			len++;
-
 		array[i] = malloc(sizeof(char) * (len + 1));
 
 		if (array[i] == NULL)
@@ -68,13 +65,11 @@ char **strtow(char *str)
 			free(array);
 			return (NULL);
 		}
-
 		for (k = 0; k < len; k++)
 			array[i][k] = str[j + k];
 		array[i][k] = '\0';
 		j += len;
 	}
-
 	array[i] = NULL;
 	return (array);
 }
