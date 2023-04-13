0be626cd4395140ea1191e6adb498af81c1c0791
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-14 01:18:59 +0200

    Advanced Task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
