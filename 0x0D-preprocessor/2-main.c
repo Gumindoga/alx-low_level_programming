01289c6ae9fc6319ac6397507ac682dbbd55ff86
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-18 01:42:20 +0200

    Tasks 0 to 4, first take

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - writes the name of teh file it was compiled from
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char *str = __FILE__;
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	
+	_putchar('\n');
+	return (0);
+}
