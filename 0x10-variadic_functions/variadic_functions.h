30e5cdc60e3115a2bdab5bbb279f440456d4d886
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-04-20 22:48:33 +0200

    Correction of task 3, adding propotypes to header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif /* VARIADIC_FUNCTIONS_H */
