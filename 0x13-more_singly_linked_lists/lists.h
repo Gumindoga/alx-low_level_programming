954069598a48e9ef6f603e7fc0311e69277e4d02
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-05-04 04:58:18 +0200

    correction on task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 void print_number(int n);
@@ -18,18 +32,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- * Description: singly linked list node structure
- */
-
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
 
 #endif /* LISTS_H */
