4d0258088c8dd06322dafa8f274ce15257639389
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-05-04 20:55:42 +0200

    Correction to Task 5, betty doc

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another
+ * flip_bits - returns the number of bits
+ * you would need to flip to get from one number to another
  * @n: the first number
  * @m: the second number
  * Return: the number of bits you would need to flip to get from n to m
