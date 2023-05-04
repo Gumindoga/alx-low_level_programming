a74ad904ca38dadb6291d31a8c4e6f58c4c35698
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-05-04 20:53:17 +0200

    Correction to Task 0, betty doc

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,9 +3,7 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number,
- * or 0 if there is one or more chars in the string b
- * that is not 0 or 1 or b is NULL
+ * Return: the converted number, 0 otherwise
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -13,12 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (!b)
-{		return (0);}
+	{
+		return (0);
+	}
 
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
-			{return (0);}
+		{
+			return (0);
+		}
 		num = num * 2 + (*b++ - '0');
 	}
 
