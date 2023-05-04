6a4c42abf99880f1a88f549a26d18cdbc34758c8
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-05-04 05:33:28 +0200

    Task O

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,7 +3,7 @@
 /**
  * sum_listint - returns the sum of all the data (n) of a listint_t linked list
  * @head: pointer to the head of the linked list
- * Return: sum of all the data (n) of the linked list, or 0 if the list is empty
+ * Return: sum of all the data (n) of the linked list, 0 otherwise
  */
 
 int sum_listint(listint_t *head)
