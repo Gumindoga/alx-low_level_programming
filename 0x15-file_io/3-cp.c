e4a3efa0fedd9e6fd64d4be40fff8c1760e848fc
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-05-12 13:15:25 +0200

    Betty, tasks 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,6 +8,51 @@
 #define BUFSIZE 1024
 
 /**
+ * error_close - prints an error message and exits when close fails
+ * @fd: file descriptor to close
+ */
+
+void error_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * error_read - prints an error message and exits when read fails
+ * @file: name of the file that could not be read
+ * @fd_from: file descriptor of the source file
+ * @fd_to: file descriptor of the destination file
+ */
+
+void error_read(char *file, int fd_from, int fd_to)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	if (close(fd_from) == -1)
+		error_close(fd_from);
+	if (close(fd_to) == -1)
+		error_close(fd_to);
+	exit(98);
+}
+
+/**
+ * error_write - prints an error message and exits when write fails
+ * @file: name of the file that could not be written to
+ * @fd_from: file descriptor of the source file
+ * @fd_to: file descriptor of the destination file
+ */
+
+void error_write(char *file, int fd_from, int fd_to)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	if (close(fd_from) == -1)
+		error_close(fd_from);
+	if (close(fd_to) == -1)
+		error_close(fd_to);
+	exit(99);
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of command-line arguments
  * @argv: array of command-line arguments
@@ -32,51 +77,27 @@ int main(int argc, char *argv[])
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		error_read(file_from, fd_from, fd_to);
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(fd_from);
-		exit(99);
-	}
+		error_write(file_to, fd_from, fd_to);
 
 	while ((n_read = read(fd_from, buffer, BUFSIZE)) > 0)
 	{
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
+			error_write(file_to, fd_from, fd_to);
 	}
 
 	if (n_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
+		error_read(file_from, fd_from, fd_to);
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
-		exit(100);
-	}
+		error_close(fd_from);
 
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_close(fd_to);
 
 	return (0);
 }
