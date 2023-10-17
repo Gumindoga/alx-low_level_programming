b9b942e610b2112c7c3be3d09a9b3f74ce25be8d
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-17 20:44:50 +0200

    fix: restructure

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,30 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - sums two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
 
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
+
+/**
+ * sub - subtracts a number from another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
 
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
+
+/**
+ * mul - multiplies two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
-int divide(int a, int b)
+/**
+ * div - divides a number by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
+
+int div(int a, int b)
 {
-	if (b != 0)
-		return a / b;
-	else
-		return 0;
+	return (a / b);
 }
+
+/**
+ * mod - returns the remainder when a number is divided by another.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Integer result of operation.
+ */
 
 int mod(int a, int b)
 {
-	return a % b;
+	return (a % b);
 }
