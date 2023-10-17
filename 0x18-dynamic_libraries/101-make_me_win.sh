fa234e9d2937490817a211464de40bbab24feb53
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-17 21:31:16 +0200

    fix: add master to path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/Gumindoga/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpotlib.so
+wget -P ../ https://github.com/Gumindoga/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpotlib.so
 export LD_PRELOAD=../jackpotlib.so
