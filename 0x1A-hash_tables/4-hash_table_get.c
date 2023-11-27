1f30c360993c5938d779fd6cb62ad152dbc0babf
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-27 04:07:03 +0200

    fix: betty style max xracters/line

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
  * @ht: the hash table you want to look into
  * @key: the key you are looking for
  *
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element, or NULL otherwise
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
